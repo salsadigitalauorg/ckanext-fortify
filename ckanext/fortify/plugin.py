54355e7c5c1ac4a85c2e20f4908a3b02f6ea5d47
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2025-04-11 03:38:13 +0200

    Testing branch for uploads (#10)
    * Remove overriding actions to dissalow non image uploads
    
    * Remove auth functions
    
    * Cleanup imports

--- ckanext/fortify/plugin.py
+++ ckanext/fortify/plugin.py
@@ -3,11 +3,8 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 import logging
 
-from ckanext.fortify import helpers, validators, blueprint
-from ckanext.fortify.logic.auth import (
-    create as auth_create,
-    update as auth_update
-)
+from ckanext.fortify import validators, blueprint
+
 from ckan.lib.uploader import ALLOWED_UPLOAD_TYPES
 
 try:
@@ -50,7 +47,6 @@ class FortifyPlugin(plugins.SingletonPlugin):
 
     if asbool(config.get('ckan.fortify.block_html_resource_uploads', False)):
         plugins.implements(plugins.IUploader, inherit=True)
-        plugins.implements(plugins.IAuthFunctions)
 
         # IUploader
 
@@ -62,19 +58,6 @@ class FortifyPlugin(plugins.SingletonPlugin):
                 # Returning None will make sure it uses the CKAN default uploader ResourceUpload
                 return None
 
-        # IAuthFunctions
-
-        def get_auth_functions(self):
-            return {
-                # Create auth function overrides
-                'group_create': auth_create.fortify_group_create,
-                'organization_create': auth_create.fortify_organization_create,
-                'user_create': auth_create.fortify_user_create,
-                # Update auth function overrides
-                'group_update': auth_update.fortify_group_update,
-                'organization_update': auth_update.fortify_organization_update,
-                'user_update': auth_update.fortify_user_update,
-            }
 
     if asbool(config.get('ckan.fortify.enable_anti_csrf_tokens', False)) \
             or asbool(config.get('ckan.fortify.enable_password_policy', False)) \
